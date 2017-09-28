c84c10b65c0b2b01271ec5dee9721f8c13946cfe
Kim LeBlanc <khleblanc@gmail.com>
2017-09-28 14:01:02 -0400

    added functions and additional analyses: -added functions for the bulk of the program -added movement analyses: open only,closed only, and in closed;percent time and total number

--- ZeroMazeMovingOpen.py
+++ ZeroMazeMovingOpen.py
@@ -10,18 +10,28 @@ import numpy as np
 import os
 import sys
 dataFolder = "/Users/leblanckh/data/KO_WT_ZeroMaze_RawData"
-resultsColumns = ["Subject", "Group", "Gender", "Timestamp", "Notes", "Time in open (%)", "Time in open while moving (%)", "Number of movements", "Average duration of movements (s)", "Total time moving (s)", "Speed while moving (cm/s)", "Average velocity (cm/s)", "Number of movements in open"]
+resultsColumns = ["Subject", "Group", "Gender", "Timestamp", "Notes", "Time in Open (%)", \
+"Time in Open while moving (%)", "Time in Closed while moving (%)", "Time moving in Open only (%)",\
+ "Time moving in Closed only (%)","Number of movements", "Average duration of movements (s)", \
+ "Total time moving (s)", "Speed while moving (cm/s)", "Average Velocity (cm/s)", \
+ "Number of movements in Open", "Number of movements in Closed", "Number of Open-only movements", \
+ "Number of Closed-only movements"]
 myDataList = []
 os.chdir(dataFolder)
 FileList = os.listdir(dataFolder)
 
+NO_BASELINE_10MIN_SESSION_TRIAL_LENGTH = 601
+ONE_MINUTE_AS_FRAMES = 1798
 
-for File in FileList:
-    if not File.endswith('.xlsx'):
-        print ("skipping file named", File)
-        continue
-    df = pd.read_excel(File, header = None)
-    #extract information from the header in the raw data from Noldus
+def Extract_Header_Info(dataframe):
+    """
+    Identifies the length of the header,Extracts independent variable 
+    information from the header, and assigns the column names to the row that
+    contains the column information in the header
+    
+    inputs: dataframe
+    Returns: NumberofHeaderRows, Subject, Genotype, Gender, Timestamp, Notes, 
+    """
     NumberofHeaderRows = int(df.iloc[0,1])
     Header = df.iloc[31:NumberofHeaderRows - 3,:]
     SubjectFilters = ["Subject", "Mouse","subject", "mouse"]
@@ -37,58 +47,92 @@ for File in FileList:
     #print ("Subject:",Subject, "Genotype:", Genotype,"Gender:", Gender,"Timestamp:", Timestamp, "Notes", Notes)
     ColumnNames = df.iloc[[NumberofHeaderRows - 2],:].values.tolist()
     df.columns = ColumnNames
-   
+    return NumberofHeaderRows,Subject,Genotype,Gender,Timestamp,Notes
     
-    #set up a data block to operate on and clean the data
-    if df['Trial time'].iloc[-1] > 601:
-        DataBlock = df.loc[NumberofHeaderRows +1801:,'Trial time':'results']
+def Create_DataBlock(dataframe, cutoffTimeSeconds, shortTimeIdx, longTimeIdx):
+    """
+    Creates a dataFrame, theData, that contains the actual raw data values.
+    It also replaces missing values in the movement column of data with NaN, then interpolates
+    returns: theData, movement column data (isMovingData), LED on column (LEDon)
+    """
+    finalColumnName = dataframe.columns[-1]
+    initialColumnName = dataframe.columns[0]
+
+    if dataframe['Trial time'].iloc[-1] <cutoffTimeSeconds:
+        theData = dataframe.loc[shortTimeIdx:,initialColumnName:finalColumnName] 
     else:
-        DataBlock = df.loc[NumberofHeaderRows +1:,'Trial time':'results']
-    firstRow = DataBlock[:1]
-    lastRow = DataBlock[-1:]
+        theData = dataframe.loc[longTimeIdx:,initialColumnName:finalColumnName] 
     
-    firstRow[firstRow == '-'] = 0
-    lastRow[lastRow == '-'] = 0
-    
-    DataBlock[:1] = firstRow
-    DataBlock[-1:] = lastRow
+    theData[:1].replace('-',0,inplace = True)
+    theData[-1:].replace('-',0,inplace = True)
      #replace missing data (-) with NaN, then interpolate
-    DataBlock.replace('-',np.NaN,inplace = True)
-    DataBlock.interpolate(method = 'values', axis = 0, inplace = True)
-    isMovingData = DataBlock.loc[:,'Movement(Moving / center-point)']    
-    isMovingData[isMovingData >= 0.5] = 1
-    isMovingData[isMovingData < 0.5] = 0
-    isInOpen = DataBlock.loc[:,'In open']
-    isInOpen[isInOpen >= 0.5] = 1
-    isInOpen[isInOpen < 0.5] = 0
-    Velocity = DataBlock.loc [:, 'Velocity']
+    theData.replace('-',np.NaN,inplace = True)
+    theData.interpolate(method = 'values', axis = 0, inplace = True)
+    theData.loc[:,'Movement(Moving / center-point)'] = Binary_Data_Interpolation_CleanUp(theData.loc[:,'Movement(Moving / center-point)']) 
+    theData.loc[:,'In open'] = Binary_Data_Interpolation_CleanUp(theData.loc[:,'In open']) 
     
-    PercentTimeinOpen = sum (isInOpen[isInOpen == 1])/len(isInOpen)*100
-    AvgVelocity = sum (Velocity)/len(Velocity)
+    return theData
     
+def Binary_Data_Interpolation_CleanUp(binaryDataColumn):
+    """
+    After interpolation, binary data columns need decimal point values converted to 0s or 1s.
+    This function sets all values 0.5 and greater to 1, and all values less than 0.5 to 0.
+    Inputs: the binary data column with decimal values
+    Returns: the binary data column with 0s and 1s only
+    """
     
-    #slice the data based on the starting and ending rows for movement
-    isMovingData.iat[0] = 0
-    isMovingData.iat[-1] = 0
-    movingTrans = isMovingData.diff()
-    startingPoints = movingTrans[movingTrans == 1].index.tolist()
-    endingPoints = movingTrans[movingTrans == -1].index.tolist()
+    binaryDataColumn[binaryDataColumn >= 0.5] = 1
+    binaryDataColumn[binaryDataColumn < 0.5] = 0
+    
+    return binaryDataColumn
+
+def Binary_Data_Transition_Point_Finder(binaryDataColumn):
+    """
+    Uses a diff method to find when 0 changes to 1 and vice versa in binary data column
+    Defines these transitions as either the starting point or ending point of an action or state, respectively
+    and sends that index value to a list
+    Also sets the first and last value of the column to 0 for edge case handling
+    
+    input: binaryDataColumn
+    Returns:  a list of binaryDataStartPoints and binaryDataEndPoints
+    """
+    binaryDataColumn.iat[0] = 0
+    binaryDataColumn.iat[-1] = 0
+    binaryDataTrans = binaryDataColumn.diff()
+    binaryDataStartingPoints = binaryDataTrans[binaryDataTrans == 1].index.tolist()
+    binaryDataEndingPoints = binaryDataTrans[binaryDataTrans == -1].index.tolist()
+    return binaryDataStartingPoints, binaryDataEndingPoints
+    
+def Movement_Analysis(InZoneColumn,VelocityColumn,MoveStart,MoveEnd):
+    """
+    Sets up the initial conditions, loops over the movement data and counts movement blocks,
+    duration and velocity,identifies movements into the center, and runs all of the calculations
+    
+    inputs: inZoneColumn, Velocity Column, and the starting and ending points of the moving column
+    returns: PercentTimeinCenter, PercentTimeinCenterMoving,MovementBlocks, AvgMoveDuration, 
+    TotalMoveDuration, AvgVelocityMoving, AvgVelocity,CenterTrue
+    """
     OpenTrue = 0
+    OpenOnly = 0
+    ClosedOnly = 0
+    ClosedTrue = 0
     inOpenCutOff = 15
     FramesOpenMoving = 0
+    FramesOpenOnly = 0
+    FramesClosedOnly = 0
+    FramesClosedMoving = 0
     MovementBlocks = 0
     TotalMoveDuration = 0
     TotalVelocity = 0
     TotalFramesMoving = 0
-   
     
-    #loop over the movement data and identify occurances of mouse entering open
-    if len(startingPoints) != len(endingPoints):
-        print ("Uneven start and end pairs. There are", (len(startingPoints)), "starting points and" , (len(endingPoints)) ,"endingPoints" )
-    for i in range(len(startingPoints)):
-        currentStart = startingPoints[i]
-        currentEnd = endingPoints[i] - 1
-        #NOTE: endingPoints are one frame beyond the end of the movement.  To correct for this, I have adjusted the current end point back one frame.
+    #loop over the movement data and identify if mouse enters Open
+    if len(MoveStart) != len(MoveEnd):
+        print ("Uneven start and end pairs. There are", (len(MoveStart)), "starting points and" , (len(MoveEnd)) ,"MoveEnd" )
+    for i in range(len(MoveStart)):
+        currentStart = MoveStart[i]
+        currentEnd = MoveEnd[i] - 1
+        #NOTE: MoveEnd are one frame beyond the end of the movement.  To correct for this, I have adjusted the current end point back one frame.
         MovementBlocks += 1
         MovementDuration = (currentEnd-currentStart)/30
         TotalMoveDuration = TotalMoveDuration + MovementDuration
@@ -98,22 +142,65 @@ for File in FileList:
         TotalVelocity = TotalVelocity + MoveVelocity
         isOpenSpan = isInOpen[currentStart:currentEnd]
         numOpen = len(isOpenSpan[isOpenSpan == 1])
+        numClosed = len(isOpenSpan[isOpenSpan == 0])
+        
+        OpenThreshold = 0.95
+        ClosedThreshold = 0.05
+        PercentOpen = numOpen/FramesMoving
+        
+        if PercentOpen >=OpenThreshold:
+            OpenOnly +=1
+            FramesOpenOnly = FramesOpenOnly + numOpen
+        if PercentOpen <=ClosedThreshold:
+            ClosedOnly +=1
+            FramesClosedOnly = FramesClosedOnly + numClosed
         if numOpen > inOpenCutOff:
             OpenTrue +=1
             FramesOpenMoving = FramesOpenMoving + numOpen
+        if numClosed > inOpenCutOff:
+            ClosedTrue +=1
+            FramesClosedMoving = FramesClosedMoving + numClosed
+            
     PercentTimeinOpenMoving = FramesOpenMoving/len(isInOpen)*100
+    PercentTimeinClosedMoving = FramesClosedMoving/len(isInOpen)*100
+    PercentTimeOpenOnly = FramesOpenOnly/len(isInOpen)*100
+    PercentTimeClosedOnly = FramesClosedOnly/len(isInOpen)*100
     AvgMoveDuration = TotalMoveDuration/MovementBlocks
     AvgVelocityMoving = TotalVelocity/TotalFramesMoving
+    PercentTimeinOpen = sum (isInOpen[isInOpen == 1])/len(isInOpen)*100
+    AvgVelocity = sum (Velocity)/len(Velocity)
+    return PercentTimeinOpen, PercentTimeinOpenMoving, \
+    PercentTimeinClosedMoving, PercentTimeOpenOnly, PercentTimeClosedOnly, \
+    MovementBlocks, AvgMoveDuration,TotalMoveDuration, AvgVelocityMoving, \
+    AvgVelocity, OpenTrue, ClosedTrue, OpenOnly, ClosedOnly
 
+for File in FileList:
+    if not File.endswith('.xlsx'):
+        print ("skipping file named", File)
+        continue
+    df = pd.read_excel(File, header = None)
     
-    
-    Dataz = [Subject,Genotype,Gender, Timestamp, Notes, PercentTimeinOpen, PercentTimeinOpenMoving, MovementBlocks, AvgMoveDuration, TotalMoveDuration, AvgVelocityMoving, AvgVelocity, OpenTrue]
+    NumHead,Sbj,Gt,Sex,DateTime,Note = Extract_Header_Info(df)
+    NB_10MIN = NumHead + 1
+    ONE_MIN_BASE = NumHead + 1801
+    DataBlock = Create_DataBlock(df, NO_BASELINE_10MIN_SESSION_TRIAL_LENGTH,NB_10MIN,ONE_MIN_BASE)
+    isMovingData = DataBlock.loc[:,'Movement(Moving / center-point)']
+    isInOpen = DataBlock.loc[:,'In open']
+    Velocity = DataBlock.loc [:, 'Velocity']
+    startingPoints,endingPoints = Binary_Data_Transition_Point_Finder(isMovingData)
+    PerTimeOpen, PerTimeOpenMove,PerTimeClosedMove,PerTimeOpenOnly,PerTimeClosedOnly,\
+    Moves,AvgMoveTime,TotalMoveTime,AvgVelMove,AvgVel,inOpen, inClosed, inOpenOnly, InClosedOnly \
+    = Movement_Analysis(isInOpen,Velocity,startingPoints,endingPoints)
+   
+    Dataz = [Sbj,Gt, Sex, DateTime, Note, PerTimeOpen, PerTimeOpenMove,PerTimeClosedMove, \
+    PerTimeOpenOnly, PerTimeClosedOnly, Moves, AvgMoveTime, TotalMoveTime, AvgVelMove, AvgVel, \
+    inOpen, inClosed, inOpenOnly, InClosedOnly]
     myDataList.append(Dataz)
     print (myDataList)
-    
+  
 resultsDF = pd.DataFrame(data = myDataList, columns=resultsColumns)
 os.chdir("/Users/leblanckh/data/KO_WT_ZeroMaze_RawData/OutputFiles")
-writer = pd.ExcelWriter('KO_and_WT_Zero_Maze_Movement_Analysis.xlsx')
+writer = pd.ExcelWriter('KO_and_WT_Zero_Maze_Movement_Analysis2.xlsx')
 resultsDF.to_excel(writer,'Sheet1')
 writer.save()
 
